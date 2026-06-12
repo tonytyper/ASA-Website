0663fd06a825d0bb1b3abcd702720e50741d1308
Narek Tonoyan <supersiblings03@gmail.com>
2026-06-12 02:49:34 -0700

    configured globals.css and layout.tsx with custom fonts & colors

--- asa-website/app/layout.tsx
+++ asa-website/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playfair_Display, Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const playfair = Playfair_Display({
+  variable: "--font-playfair",
+  subsets: ['latin']
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const lato = Lato({
+  weight: ['300', '400', '700'],
+  variable: "--font-lato",
+  subsets: ['latin']
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${playfair.variable} ${lato.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
