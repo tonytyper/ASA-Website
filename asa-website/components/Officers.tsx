bc811b53b0a267cd8996ef478912c1aae6486e21
Narek Tonoyan <supersiblings03@gmail.com>
2026-06-17 03:09:43 -0700

    implemented reactive officer cards with images and descriptions as proof of concept

--- asa-website/components/Officers.tsx
+++ asa-website/components/Officers.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Officers(){
     return (
       <>
@@ -11,58 +13,50 @@ export default function Officers(){
           <div className="officers-grid">
             <div className="officer-card">
               <div className="officer-avatar">
-                <div className="officer-initials">P</div>
+                <Image src="/officers/p25.PNG" alt="Ariana Khzarjyan, President" fill sizes="240px" style={{ objectFit: "cover" }} />
               </div>
               <div className="officer-info">
                 <div className="officer-name">President</div>
-                <div className="officer-role">Executive Board</div>
+                <div className="officer-role">Ariana Khzarjyan</div>
               </div>
             </div>
             <div className="officer-card">
               <div className="officer-avatar">
-                <div className="officer-initials">VP</div>
+                <Image src="/officers/vp25.PNG" alt="Mary Makaryan, Vice President" fill sizes="240px" style={{ objectFit: "cover" }} />
               </div>
               <div className="officer-info">
                 <div className="officer-name">Vice President</div>
-                <div className="officer-role">Executive Board</div>
+                <div className="officer-role">Mary Makaryan</div>
               </div>
             </div>
             <div className="officer-card">
               <div className="officer-avatar">
-                <div className="officer-initials">S</div>
+                <Image src="/officers/s25.PNG" alt="Martin Mnatsakanyan, Secretary" fill sizes="240px" style={{ objectFit: "cover" }} />
               </div>
               <div className="officer-info">
                 <div className="officer-name">Secretary</div>
-                <div className="officer-role">Executive Board</div>
+                <div className="officer-role">Martin Mnatsakanyan</div>
               </div>
             </div>
             <div className="officer-card">
               <div className="officer-avatar">
-                <div className="officer-initials">T</div>
+                <Image src="/officers/t25.PNG" alt="Samvel Janvelyan, Treasurer" fill sizes="240px" style={{ objectFit: "cover" }} />
               </div>
               <div className="officer-info">
                 <div className="officer-name">Treasurer</div>
-                <div className="officer-role">Executive Board</div>
+                <div className="officer-role">Samvel Janvelyan</div>
               </div>
             </div>
             <div className="officer-card">
               <div className="officer-avatar">
-                <div className="officer-initials">PR</div>
+                <Image src="/officers/h25.PNG" alt="Mane Hovhannisyan, Historian" fill sizes="240px" style={{ objectFit: "cover" }} />
               </div>
               <div className="officer-info">
-                <div className="officer-name">Public Relations</div>
-                <div className="officer-role">Officer</div>
+                <div className="officer-name">Historian</div>
+                <div className="officer-role">Mane Hovhannisyan</div>
               </div>
             </div>
-            <div className="officer-card">
-              <div className="officer-avatar">
-                <div className="officer-initials">EO</div>
-              </div>
-              <div className="officer-info">
-                <div className="officer-name">Events Officer</div>
-                <div className="officer-role">Officer</div>
-              </div>
-            </div>
+
           </div>
         </section>
 
