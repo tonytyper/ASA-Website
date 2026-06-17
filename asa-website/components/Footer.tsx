36a7b4bbeea960589f4f68e2652bc338196e26f3
Narek Tonoyan <supersiblings03@gmail.com>
2026-06-17 03:24:29 -0700

    created footer with copyright statement & easter egg

--- asa-website/components/Footer.tsx
+++ asa-website/components/Footer.tsx
@@ -1,7 +1,8 @@
-export default function Footer(){
+export default function Footer() {
     return (
-        <section id = "footer">
-            
+        <section id="footer">
+            <p>&copy; {new Date().getFullYear()} Narek (Tony) Tonoyan. All rights reserved.</p>
+            <p>լավ ես՞ ապե</p>
         </section>
     )
 }
